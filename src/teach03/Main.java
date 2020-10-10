c9a03971ad8b9cfb4b44758ee3d8e414188e2156
Daijiro Sagane <daijiro.2812@i.softbank.jp>
2020-10-10 20:23:52 +0900

    Modefied text

--- src/teach03/Main.java
+++ src/teach03/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, my name is Yuki.");
+        System.out.println("Hello Yuki, my name is Daijiro");
     }
 }
